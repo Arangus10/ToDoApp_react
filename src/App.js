f8fd3cd63e64632b29846e3be092a65b26ff3f5e
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2022-12-05 14:48:44 +0100

    Install Webpack & Add Hero.js & SearchForm.js & List.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hero from './components/Hero/Hero'
+import SearchForm from './components/SearchForm/SearchForm'
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+    </div>
+  );
+};
+
+export default App;
