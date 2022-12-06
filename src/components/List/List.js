bbd9c20dd5a076eeb3f53f60abc793fa65c19adf
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2022-12-06 10:52:55 +0100

    Add List.module.scss & SearchForm.module.scss & Change Hero.module.scss

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,15 +6,15 @@ const List = () => {
           </header>
           <p>Interesting things I want to check out</p>
           <section>
-              <article>
-                  <h2>Books</h2>
-              </article>
-              <article>
-                  <h2>Movies</h2>
-              </article>
-              <article>
-                  <h2>Games</h2>
-              </article>
+            <article>
+                <h2>Books</h2>
+            </article>
+            <article>
+                <h2>Movies</h2>
+            </article>
+            <article>
+                <h2>Games</h2>
+            </article>
           </section>
       </div>
     );
