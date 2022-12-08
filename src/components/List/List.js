0de594f2e67b01c3624e26aabd99f7f77e0b4840
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2022-12-08 09:59:22 +0100

    Add .map() @ List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,15 @@
 import styles from "./List.module.scss";
 import Column from "../Column/Column";
-import SearchForm from '../SearchForm/SearchForm';
+
 
 const List = () => {
+
+    const columns = [
+      { id: 1, title: 'Books', icon: 'book' },
+      { id: 2, title: 'Movies', icon: 'film' },
+      { id: 3, title: 'Games', icon: 'gamepad' }
+    ];
+
     return (
       <div className={styles.list}>
           <header className={styles.header}>
@@ -10,9 +17,9 @@ const List = () => {
           </header>
           <p className={styles.description}>Interesting things I want to check out</p>
           <section className={styles.columns}>
-            <Column title="Books" icon='book'/>
-            <Column title="Movies" icon='gamepad'/>
-            <Column title="Games" icon='film'/>
+              {columns.map(column => 
+                  <Column key={column.id} title={column.title} icon={column.icon} />
+              )}
           </section>
       </div>
     );
