9aeb3e46e8634c33300e1fa68031444239c11113
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2022-12-16 14:37:36 +0100

    Add action ADD_COLUMN & ADD_CARD @ reducer()

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,20 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
+import shortid from 'shortid';
 
 const reducer = (state, action) => {
-    if(action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, action.newColumn]}
-    return state;
+    switch(action.type) {
+      case 'ADD_COLUMN': 
+        return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
+        break;
+      case 'ADD_CARD':
+        return {...state, cards: [...state.cards, {...action.payload, id:shortid() }]};
+        break;
+      default:
+        return state;
+    }
   };
+  
   
 
 const store = createStore(
